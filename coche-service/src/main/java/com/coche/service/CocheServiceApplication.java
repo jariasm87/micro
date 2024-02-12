a52605fa7a4a7f3ab818b5d353310f01baec519c
Jose Arias <jariasm@etics.es>
2024-02-12 13:47:42 +0100

    eureka

--- coche-service/src/main/java/com/coche/service/CocheServiceApplication.java
+++ coche-service/src/main/java/com/coche/service/CocheServiceApplication.java
@@ -2,8 +2,10 @@ package com.coche.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CocheServiceApplication {
 
 	public static void main(String[] args) {
